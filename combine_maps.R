c263cb2f865d7fc97de4241f9394e177673eeb70
florian gollnow <floriangollnow@florians-MBP.home>
2020-12-16 15:24:37 +0100

    combined map

--- combine_maps.R
+++ combine_maps.R
@@ -1,7 +1,4 @@
 
-
-
-
 # combine all maps 
 library(tidyverse)
 library(ggpubr)
@@ -18,24 +15,11 @@ gg6<-read_rds(file.path(plot.dir, "gg_title.rds") )
 gg7<-read_rds(file.path(plot.dir, "gg_tech.rds") )
 gg8<-read_rds(file.path(plot.dir, "gg_world.rds"))
 
-ggarrange(gg1,gg2,gg3,gg4,gg5,gg6,gg7,gg8,ncol=2,nrow=4, align = "h", labels=c("a","b","c","d","e","f","g","h"))
-ggsave  (file.path  (plot.dir, "combined.png"), width = 10.8*2.8, height = 11.5*6, scale = 0.25)
+#ggarrange(gg1,gg2,gg3,gg4,gg5,gg6,gg7,gg8,ncol=2,nrow=4, align = "h", labels=c("a","b","c","d","e","f","g","h"))
+#ggsave  (file.path  (plot.dir, "combined.png"), width = 10.8*2.8, height = 11.5*6, scale = 0.25)
 
-
-# main land use in 2019 (MapBiomas v5) + Indigenous and protected areas (IBGE)
-# soy areas in 2019 (MapBiomas v5) + Indigenous and protected areas (IBGE)
-# ZDC (ZDC in 2018 of all companies that pledged to eliminate deforestation, Trase 25)
-# mean monthly income 2010 (censo demographico 2010)
-# gini coeficient 2010 (censo demograpico 2010)
-# % land owned (other categories included - censo agropecuaria 2017)
-## [1] "Total"                                                            
-## [2] "Proprietário(a)" <- owned!
-## [3] "Concessionário(a) ou assentado(a) aguardando titulação definitiva"
-## [4] "Arrendatário(a)"
-## [5] "Parceiro(a)"
-## [6] "Comodatário(a)"
-## [7] "Ocupante"
-## [8] "Produtor sem área"
-# % of farms that received technical assistance (censo agropecuaria 2017))
-# global overview
-
+ggarrange(ggarrange(gg1,gg2,ncol = 2, labels=c("a","b")),
+          ggarrange(gg3,gg4,gg5,ncol=3, labels=c("c","d","e")),
+          ggarrange(gg6,gg7,gg8,ncol=3, labels=c("f","g","h")),
+          nrow=3)
+ggsave  (file.path  (plot.dir, "combined_test3.png"), width = 10.8*4, height = 11.5*6, scale = 0.25)
